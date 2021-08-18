932fd8e5fed2f1ba32f22d3aa01025be2159d3f3
derochs <ochs.marcel@googlemail.com>
2021-08-18 14:48:48 +0200

    Change eval name

--- trainer.py
+++ trainer.py
@@ -70,8 +70,8 @@ class Trainer(object):
                 )
             evals.append(evaluate.eval(
                 os.path.join(self.output_dir, f"{self.model_name}-{epoch}.pt"),
-                "output/test_dps.txt",
-                "output/test_ids.txt",
+                "output/eval_dps.txt",
+                "output/eval_ids.txt",
                 epoch=epoch
             ))
             with open(os.path.join(self.output_dir, "evals.pickle"), "wb") as fout:
