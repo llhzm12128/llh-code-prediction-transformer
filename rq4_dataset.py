0daa388aefe72ef3277845662d2ae8e2319f9638
derochs <ochs.marcel@googlemail.com>
2021-09-02 15:14:26 +0200

    RQ4

---
+++ rq4_dataset.py
@@ -0,0 +1,42 @@
+import torch
+import utils
+import json
+
+class Dataset(torch.utils.data.Dataset):
+    def __init__(self, fp):
+        super().__init__()
+        self.fp = fp
+        self._line_pos_dp = list(utils.line_positions(fp))
+    
+    def __len__(self):
+        return len(self._line_pos_dp)
+    
+    def __getitem__(self, idx):
+        line_pos = self._line_pos_dp[idx]
+        with open(self.fp) as f:
+            f.seek(line_pos)
+            dp_line = f.readline().strip()
+        return json.loads(dp_line)
+
+    @staticmethod
+    def collate(seqs, pad_idx=None):
+        max_len = max(len(seq[0]) for seq in seqs)
+        max_len = max(max_len, 2)
+        input_seqs = []
+        target_seqs = []
+        extended = []
+
+        for i,  (seq, ext) in enumerate(seqs):
+            print("i: {}".format(i))
+            print("seq: {}".format(seq))
+            print("ext: {}".format(ext))
+            padding = [pad_idx] * (max_len - len(seq))
+            input_seqs.append(seq[:-1] + padding)
+            target_seqs.append(seq[1:] + padding)
+            extended.append(ext)
+
+        return {
+            "input_seq": torch.tensor(input_seqs),
+            "target_seq": torch.tensor(input_seqs),
+            "extended": torch.tensor(extended)
+        }
--- tokenizer.py
+++ tokenizer.py
@@ -3,7 +3,10 @@ from tokenizers.models import WordPiece
 from tokenizers.trainers import WordPieceTrainer
 from tokenizers.pre_tokenizers import CharDelimiterSplit
 
+from utils import file_tqdm, separate_dps
+
 import json
+import logging
 from tqdm import tqdm
 
 # Clean up raw py150 dataset by removing "," character
@@ -26,7 +29,7 @@ if False:
                     if "," in d["value"]:
                         print('Not cleaned up')
 
-# Extract value/types from trees and store in comma separated raw file
+# Extract value/types from trees and store in comma separated raw file (all_raw.json)
 
 if False:
     with open("output/all_new_trees.json") as fin, open("output/all_raw.json", "w") as fout:
@@ -43,10 +46,55 @@ if False:
 
 # Train tokenizer on raw file
 
-tokenizer = Tokenizer(WordPiece(unk_token="[UNK]"))
-tokenizer.pre_tokenizer = CharDelimiterSplit(delimiter=",")
-trainer = WordPieceTrainer(special_tokens=["[UNK]", "[PAD]"])
+#tokenizer = Tokenizer(WordPiece(unk_token="[UNK]"))
+# tokenizer.pre_tokenizer = CharDelimiterSplit(delimiter=",")
+# trainer = WordPieceTrainer(special_tokens=["[UNK]", "[PAD]"])
 
-tokenizer.train(["output/all_raw.json"], trainer)
+# tokenizer.train(["output/all_raw.json"], trainer)
 
-tokenizer.save("output/tokenizer.json")
+# tokenizer.save("output/tokenizer.json")
+
+def split(ast, max_len, tokenizer):
+    # list contains leaf ID and encoded tokens    
+    d = []
+
+    # Iterate through tree nodes, fill list
+    for i, a in enumerate(ast):
+        if "type" in a:
+            ids = tokenizer.encode(a["type"]).ids
+            d.extend(ids)
+        elif "value" in a:
+            ids = tokenizer.encode(a["value"]).ids
+            d.extend(ids)
+
+    # print("d len: {}".format(len(d)))
+    
+    half_len = int(max_len / 2)
+    if len(d) <= max_len:
+        return [[d, 0]]
+
+    aug_d = [[d[:max_len], 0]]
+    # print("first: {}\n\n".format(aug_d[-1]))
+    i = half_len
+    while i < len(d) - max_len:
+        aug_d.append([d[i : i + max_len], half_len])
+        # print("loop: {}\n\n".format(aug_d[-1]))
+        i += half_len
+    idx = max_len - (len(d) - (i + half_len))
+    aug_d.append([d[-max_len:], idx])
+    # print("last: {}\n\n".format(aug_d[-1]))
+    return aug_d
+
+tokenizer = Tokenizer.from_file("output/tokenizer.json")
+outfile = "output/train_rq4_dps.txt"
+num_dps = 0
+with open("output/train_new_trees.json") as fin, open(outfile, "w") as fout:
+    for line in file_tqdm(fin):
+        dp = json.loads(line.strip())
+        asts = split(dp, 1000, tokenizer)
+        for ast, extended in asts:
+            if len(ast) > 1:
+                json.dump([ast, extended], fp=fout)
+                fout.write("\n")
+                num_dps += 1
+logging.info("Wrote {} datapoints to {}".format(num_dps, outfile))
