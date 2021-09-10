daacc88dafd5dd20cc85f65202fc573802b0a20a
derochs <ochs.marcel@googlemail.com>
2021-09-10 16:06:38 +0200

    Evaluate new tokenizer

--- rq4_evaluate.py
+++ rq4_evaluate.py
@@ -19,32 +19,71 @@ def main():
 
     eval(args.model, args.dps, args.ids, tokenizer)
 
+def mean_reciprocal_rank(labels, predictions):
+    scores = []
+    for i, l in enumerate(labels):
+        score = 0
+        for j, p in enumerate(predictions[i]):
+            if l == p:
+                score = 1 / (j + 1)
+                break
+        scores.append(score)
+    return sum(scores) / len(scores)
+
 def eval(model_fp, dps, ids, tokenizer):
-    dataset = Dataset(dps)
+    dataset = Dataset(dps, ids)
     pad_idx = tokenizer.encode("[PAD]").ids[0]
-    m = model.from_file(model_fp, tokenizer.get_vocab_size(), pad_idx)
     dataloader = torch.utils.data.DataLoader(
         dataset,
         batch_size=1,
         collate_fn = lambda b: dataset.collate(b, pad_idx)
     )
+    m = model.from_file(model_fp, tokenizer.get_vocab_size(), pad_idx)
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     m = m.to(device)
     m.eval()
-
     print("Evaluating {} batches".format(len(dataloader)))
+    string_accuracy = []
     for i, batch in tqdm(enumerate(dataloader)):
         with torch.no_grad():
-            if i == 0:
-                x = batch["input_seq"][0]
-                y = batch["target_seq"][0]
-                x = x.to(device)
-                output = m(x, None)
-                for j in range(len(y)):
-                    print("y @{}: {}".format(j, tokenizer.decode([y[j].item()])))
-                    print("y_predict top 10 @{}: {}\n".format(j, [tokenizer.decode([c.item()]) for c in torch.topk(output[j], 10).indices]))
-            else:
-                return
+            x = batch["input_seq"][0]
+            y = batch["target_seq"][0]
+            # Subtract 1 to get prediction for string ids
+            string_ids = [a - 1 for a in batch["ids"]["string_ids"] if a > 0]
+            # print(string_ids)
+
+            x = x.to(device)
+            output = m(x, None)
+            
+            iter_string_mrrs = []
+
+            for id in string_ids:
+                # Counter for max string length to prevent inf loop, until 20
+                breaker = 20
+                counter = 0
+                string_mrrs = []
+                # print("ID: {}".format(id))
+                while counter < breaker:
+                    # print("\tCounter: {}".format(counter))
+                    if counter == 0:
+                        label = y[id + counter]
+                        top_10 = torch.topk(output[id + counter], 10)[1]
+                        string_mrrs.append(mean_reciprocal_rank([label.item()], [top_10.tolist()]))
+                        # print("\tLabel: {}\n\tTop10: {}".format(label, ", ".join([str(t.item()) for t in top_10])))
+                    else:
+                        if id + counter < len(y):
+                            label = y[id + counter]
+                            top_10 = torch.topk(output[id + counter], 10)[1]
+                            if not tokenizer.decode([top_10[0]]).startswith("#"):
+                                # print("\tNext token no subword continuation, breaking")
+                                break
+                            string_mrrs.append(mean_reciprocal_rank([label.item()], [top_10.tolist()]))
+                            # print("\tLabel: {}\n\tTop10: {}".format(label, ", ".join([str(t.item()) for t in top_10])))
+                    counter += 1 
+                iter_string_mrrs.append(sum(string_mrrs) / len(string_mrrs))
+            if len(iter_string_mrrs) > 0:
+                string_accuracy.append(sum(iter_string_mrrs) / len(iter_string_mrrs))
+    print("String accuracy: {}".format(sum(string_accuracy) / len(string_accuracy)))
 
 if __name__ == "__main__":
     main()
--- rq4_preprocess.py
+++ rq4_preprocess.py
@@ -4,6 +4,7 @@ import json
 import logging
 
 import generate_new_trees
+import rq4_generate_ast_ids
 
 from tokenizers import Tokenizer
 from tqdm import tqdm
@@ -22,50 +23,120 @@ def main():
     # Remove comma character from trees
     print("Removing \",\" character from new trees")
     clean_trees("output/{}_new_trees.json".format(args.suffix), args.suffix)
-    # Split trees and traverse DFS
-    print("Splitting and encoding trees")
+    # Split trees and traverse DFS, generate DPS
+    print("Splitting and encoding trees, generating IDs")
     preprocess("output/{}_new_trees_cleaned.json".format(args.suffix), args.suffix, args.tokenizer)
 
 def split(ast, max_len, tokenizer):
-    # list contains leaf ID and encoded tokens    
     d = []
+    ids = {
+        # Is node leaf or internal
+        "leaf_ids": [],
+        "internal_ids": [],
 
-    # Iterate through tree nodes, fill list
-    for i, a in enumerate(ast):
-        if "type" in a:
-            ids = tokenizer.encode(a["type"]).ids
-            d.extend(ids)
-        elif "value" in a:
-            ids = tokenizer.encode(a["value"]).ids
-            d.extend(ids)
-    
+        # Values 
+        "attr_ids": [],
+        "num_ids": [],
+        "name_ids": [],
+        "param_ids": [],
+        "string_ids": [],
+
+        # Types
+        "call_ids": [],
+        "assign_ids": [],
+        "return_ids": [],
+        "list_ids": [],
+        "dict_ids": [],
+        "raise_ids": [],
+    }
+
+    counter = 0
+
+    for i, node in enumerate(ast):
+
+        if "type" in node:
+            tokenized_ids = tokenizer.encode(node["type"]).ids
+            d.extend(tokenized_ids)
+            ids["internal_ids"].append(counter)
+            if node["type"] == "attr":
+                ids["attr_ids"].append(counter + 1)
+            elif node["type"] == "Num":
+                ids["num_ids"].append(counter + 1)
+            elif node["type"] in {"NameLoad", "NameStore"}:
+                ids["name_ids"].append(counter + 1)
+            elif node["type"] == "NameParam":
+                ids["param_ids"].append(counter + 1)
+            elif node["type"] == "Str":
+                ids["string_ids"].append(counter + 1)
+            elif node["type"] == "Call":
+                ids["call_ids"].append(counter)
+            elif node["type"] == "Assign":
+                ids["assign_ids"].append(counter)
+            elif node["type"] == "Return":
+                ids["return_ids"].append(counter)
+            elif node["type"] in {"ListComp", "ListLoad", "ListStore"}:
+                ids["list_ids"].append(counter)
+            elif node["type"] in {"DictComp", "DictLoad", "DictStore"}:
+                ids["dict_ids"].append(counter)
+            elif node["type"] == "Raise":
+                ids["raise_ids"].append(counter)    
+            counter += len(tokenized_ids)
+        elif "value" in node:
+            tokenized_ids = tokenizer.encode(node["value"]).ids
+            d.extend(tokenized_ids)
+            ids["leaf_ids"].append(counter)
+            counter += len(tokenized_ids)
+
+    id_range = list(range(len(d)))
+
     half_len = int(max_len / 2)
+
     if len(d) <= max_len:
-        return [[d, 0]]
+        return [[[d, 0]], [ids]]
 
     aug_d = [[d[:max_len], 0]]
+    aug_leaf_ids = [id_range[:max_len]]
+
     i = half_len
+
     while i < len(d) - max_len:
         aug_d.append([d[i : i + max_len], half_len])
+        aug_leaf_ids.append(id_range[i : i + max_len])
         i += half_len
+
     idx = max_len - (len(d) - (i + half_len))
     aug_d.append([d[-max_len:], idx])
-    return aug_d
+    aug_leaf_ids.append(id_range[-max_len:])
+
+    id_result_list = []
+
+    for i, aug_leaf_id_slice in enumerate(aug_leaf_ids):
+        diff = min(aug_leaf_id_slice)
+        slice_ids = dict.fromkeys(ids.keys(), [])
+        for id_key, id_values in ids.items():
+            for id_value in id_values:
+                if id_value in aug_leaf_id_slice:
+                    slice_ids[id_key].append(id_value - diff)
+        id_result_list.append(slice_ids)
+    return [aug_d, id_result_list]
 
 def preprocess(fp, suffix, tokenizer):
     tokenizer = Tokenizer.from_file(tokenizer)
-    outfile = "output/{}_dps.txt".format(suffix)
-    num_dps = 0
-    with open(fp) as fin, open(outfile, "w") as fout:
-        for line in file_tqdm(fin):
+    dps_outfile = "output/{}_dps.txt".format(suffix)
+    ids_outfile = "output/{}_ids.txt".format(suffix)
+    num = 0
+    with open(fp) as fin, open(dps_outfile, "w") as fout_dps, open(ids_outfile, "w") as fout_ids:
+        for i, line in enumerate(file_tqdm(fin)):
             dp = json.loads(line.strip())
-            asts = split(dp, 1000, tokenizer)
-            for ast, extended in asts:
+            asts, ids = split(dp, 1000, tokenizer)
+            for i, (ast, extended) in enumerate(asts):
                 if len(ast) > 1:
-                    json.dump([ast, extended], fp=fout)
-                    fout.write("\n")
-                    num_dps += 1
-    logging.info("Wrote {} datapoints to {}".format(num_dps, outfile))
+                    json.dump([ast, extended], fp=fout_dps)
+                    json.dump(ids[i], fp=fout_ids)
+                    fout_dps.write("\n")
+                    fout_ids.write("\n")
+                    num += 1
+    logging.info("Wrote {} datapoints to {} and {}".format(num, ids_outfile, dps_outfile))
 
 def clean_trees(fp, suffix):
     with open(fp) as fin, open("output/{}_new_trees_cleaned.json".format(suffix), "w") as fout:
