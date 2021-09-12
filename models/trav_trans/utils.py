0f369fa0015bbbd7add38c20ab97e76ce6256aec
derochs <ochs.marcel@googlemail.com>
2021-09-12 18:11:27 +0200

    Add overlap evaluator

--- models/trav_trans/utils.py
+++ models/trav_trans/utils.py
@@ -104,6 +104,38 @@ def separate_dps(ast, max_len):
     idx = max_len - (len(ast) - (i + half_len))
     aug_asts.append([ast[-max_len:], idx])
     return aug_asts
+    
+def rq6_separate_dps(ast, max_len, overlap = 0.5):
+    """
+    Handles training / evaluation on long ASTs by splitting
+    them into smaller ASTs of length max_len, with a sliding
+    window of max_len / 2.
+
+    Example: for an AST ast with length 1700, and max_len = 1000,
+    the output will be:
+    [[ast[0:1000], 0], [ast[500:1500], 1000], [ast[700:1700], 1500]]
+
+    Input:
+        ast : List[Dictionary]
+            List of nodes in pre-order traversal.
+        max_len : int
+
+    Output:
+        aug_asts : List[List[List, int]]
+            List of (ast, beginning idx of unseen nodes)
+    """
+    half_len = int(max_len / (1/overlap))
+    if len(ast) <= max_len:
+        return [[ast, 0]]
+
+    aug_asts = [[ast[:max_len], 0]]
+    i = half_len
+    while i < len(ast) - max_len:
+        aug_asts.append([ast[i : i + max_len], half_len])
+        i += half_len
+    idx = max_len - (len(ast) - (i + half_len))
+    aug_asts.append([ast[-max_len:], idx])
+    return aug_asts
 
 
 def get_ancestors(ast):
