928a9e19cfe7477510c799abc0844ee344f72992
derochs <ochs.marcel@googlemail.com>
2021-10-12 14:17:31 +0200

    Remove debug

--- evaluate.py
+++ evaluate.py
@@ -87,12 +87,11 @@ def eval(model_fp, dps, ids, embedding_size = 300, n_layers = 6):
     }
 
     for i, batch in tqdm(enumerate(dataloader)):
-        if i == 0:
+        if True:
             with torch.no_grad():
                 x = batch["input_seq"][0]
                 y = batch["target_seq"][0]
                 
-                print("Y: {}".format(y.size()))
 
                 x = x.to(device)
                 output = m(x, None)
